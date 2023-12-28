f0cd569c2aca1236d14e7d150443ccd7ffb8c19c
learndaws <learndaws@gmail.com>
2023-12-28 22:59:02 +0530

    First Variable script push

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "Ramesh: Hi Suresh, How are you?"
-echo "Suresh: Hey Ramesh, What's up"
-echo "Ramesh: Great to see you Suresh"
-echo "Suresh: I'm also too"
+CLIENT=Suresh
+VENDOR=Ramesh
+
+
+
+echo "$VENDOR: Hi $CLIENT, How are you?"
+echo "$CLIENT: Hey $VENDOR, What's up"
+echo "$VENDOR: Great to see you $CLIENT"
+echo "$CLIENT: I'm also too"
