21cbbf0e15e2aef1042fd93879d2dbbfff7c1172
learndaws <learndaws@gmail.com>
2023-12-29 16:33:05 +0530

    data types

--- 09-arrays.sh
+++ 09-arrays.sh
@@ -3,7 +3,5 @@
 FRUITS=("ORANGE" "GRAPES" "APPLE")
 
 echo "First Value: ${FRUITS[0]}"
-echo "First Value: ${FRUITS[1]}"
-echo "First Value: ${FRUITS[2]}"
-
-echo "First Value: ${FRUITS[0]} First Value: ${FRUITS[1]} First Value: ${FRUITS[2]}"
+echo "Second Value: ${FRUITS[1]}"
+echo "Third Value: ${FRUITS[2]}"
