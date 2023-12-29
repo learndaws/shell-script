ac9f67aeb09a8109e88cbbb7a3e0a2a942ac7e3c
learndaws <learndaws@gmail.com>
2023-12-29 16:31:29 +0530

    exp on 03-variables

--- 09-arrays.sh
+++ 09-arrays.sh
@@ -6,4 +6,7 @@ echo "First Value: ${FRUITS[0]}"
 echo "First Value: ${FRUITS[1]}"
 echo "First Value: ${FRUITS[2]}"
 
-echo "First Value: ${FRUITS[0]} First Value: ${FRUITS[1]} First Value: ${FRUITS[2]}"
+echo 
+"First Value: ${FRUITS[0]} 
+ First Value: ${FRUITS[1]} 
+ First Value: ${FRUITS[2]}"
