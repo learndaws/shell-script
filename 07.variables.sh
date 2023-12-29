13d09ecf396450a4ada03648f4476cb8fc881f97
learndaws <learndaws@gmail.com>
2023-12-29 16:13:14 +0530

    exp on 03-variables

---
+++ 07.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$3
+
+echo "$PERSON1: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hey $PERSON1, What's up"
+echo "$PERSON1: Great to see you $PERSON2"
+echo "$PERSON2: I'm also too"
