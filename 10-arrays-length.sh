1510a868794e8efb3b0a1bc738ae73a76f9b6b7c
learndaws <learndaws@gmail.com>
2023-12-29 20:57:46 +0530

    10th arrays-length command push

--- 10-arrays-length.sh
+++ 10-arrays-length.sh
@@ -2,6 +2,14 @@
 
 FRUITS=("ORANGE" "GRAPES" "APPLE" "JACK-FRUIT")
 
-array_length=${#FRUITS[ORANGE]}
+array_length=${#FRUITS[@]}
 
 echo "The length of the array is: $array_length"
+
+
+FRUITS=("ORANGE" "GRAPES" "APPLE" "JACK-FRUIT")
+
+echo "First Value: ${FRUITS[0]}"
+echo "First Value: ${FRUITS[1]}"
+echo "First Value: ${FRUITS[2]}"
+echo "First Value: ${FRUITS[3]}"
