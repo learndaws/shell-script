87da46beb695d59bca9f07af2ba8fa6fa5084964
learndaws <learndaws@gmail.com>
2023-12-30 21:20:16 +0530

    mysql-install with shell script

--- 12-mysql-install.sh
+++ 12-mysql-install.sh
@@ -8,3 +8,11 @@ echo "you don't have root access to run the script"
 exit 1
 fi
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+echo "installation failed"
+exit 1
+else
+echo "installation is success"
+fi
