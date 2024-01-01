6c7fbe50d3f2eeb1e871dbe3e8158f4518f45eba
learndaws <learndaws@gmail.com>
2024-01-01 19:56:55 +0530

    12

--- 12-mysql-install.sh
+++ 12-mysql-install.sh
@@ -7,20 +7,20 @@ EXIT_CHECK()
 {
     if [ $1 != 0 ];
         then
-            echo "FAILURE-2: $2"
+            echo "$2"
             exit 1
         else
-            echo "SUCCESS-2: $3"
+            echo "$3"
 fi
 }
 
-EXIT_CHECK "$ROOTACCESSCHECK" "you don't have root access" "you have root access"
+EXIT_CHECK "$ROOTACCESSCHECK" "FAILURE-1: you don't have root access" "SUCCESS-1: you have root access"
 
 
 yum install mysql -y
 
-EXIT_CHECK "$?" "Mysql installation is failed" "Mysql installation is success"
+EXIT_CHECK "$?" "FAILURE-2: Mysql installation is failed" "SUCCESS-2: Mysql installation is success"
 
 
 yum install git -y
-EXIT_CHECK "$?" "git installation is failed" "git installation is success"
+EXIT_CHECK "$?" "FAILURE-3: git installation is failed" "SUCCESS-3: git installation is success"
