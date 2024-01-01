1c4657f86336ab30ca52a88ada6e2549be5f50d5
learndaws <learndaws@gmail.com>
2024-01-01 15:17:00 +0530

    mysql and git installation without function

--- 12-mysql-install.sh
+++ 12-mysql-install.sh
@@ -7,20 +7,20 @@ EXIT_CHECK()
 {
     if [ $1 != 0 ];
         then
-            echo "FAILURE-2: $2 failed"
+            echo "FAILURE-2: $2"
             exit 1
         else
-            echo "SUCCESS-2: $2 success"
+            echo "SUCCESS-2: $3"
 fi
 }
 
-EXIT_CHECK "$ROOTACCESSCHECK" "root access"
+EXIT_CHECK "$ROOTACCESSCHECK" "you don't have root access" "you have root access"
 
 
 yum install mysql -y
 
-EXIT_CHECK "$?" "Mysql installation"
+EXIT_CHECK "$?" "Mysql installation is success" "Mysql installation is failed"
 
 
 yum install git -y
-EXIT_CHECK "$?" "git installation"
+EXIT_CHECK "$?" "git installation is success" "git installation is failed"
