d47f9060d88a73f620f7c9751410fc34a6678272
learndaws <learndaws@gmail.com>
2024-01-01 14:46:58 +0530

    mysql and git installation without function

--- 12-mysql-install.sh
+++ 12-mysql-install.sh
@@ -2,28 +2,30 @@
 
 ROOTACCESSCHECK=$(id -u)
 
-if [ $ROOTACCESSCHECK -ne 0 ]
-then
-echo "you don't have root access to run the script"
-exit 1
+if [ $ROOTACCESSCHECK != 0 ];
+    then
+        echo "FAILURE-1: you don't have root access to run the script"
+        exit 1
+    else
+        echo "SUCCESS-1: you have root access"
 fi
 
 yum install mysql -y
 
-if [ $? -ne 0 ]
-then
-echo "installation failed"
-exit 1
-else
-echo "installation is success"
+if [ $? != 0 ];
+    then
+        echo "FAILURE-2: Mysql installation failed"
+        exit 1
+    else
+        echo "SUCCESS-2: Mysql installation success"
 fi
 
 yum install git -y
 
-if [ $? -ne 0 ]
-then
-echo "installation failed"
-exit 1
-else
-echo "installation is success"
+if [ $? != 0 ];
+    then
+        echo "FAILURE-3: git installation failed"
+        exit 1
+    else
+        echo "SUCCESS-3: git installation success"
 fi
