5498da6dafa76c9d1cba087ff0b6240a461d7784
learndaws <learndaws@gmail.com>
2024-01-01 19:53:41 +0530

    12

--- 12-mysql-install.sh
+++ 12-mysql-install.sh
@@ -17,7 +17,7 @@ fi
 EXIT_CHECK "$ROOTACCESSCHECK" "you don't have root access" "you have root access"
 
 
-yum install myql -y
+yum install mysql -y
 
 EXIT_CHECK "$?" "Mysql installation is failed" "Mysql installation is success"
 
