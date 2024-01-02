95b72172be38db89172437ce286e3c7f90cfd77c
learndaws <learndaws@gmail.com>
2024-01-02 18:53:10 +0530

    exp push

--- 16-for-loop-with-function.sh
+++ 16-for-loop-with-function.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+FIRST=$1
+SECOND=$2
 
 FUNCTION() 
 {
-for PRINT_1TO5 in {1..5}
+for PRINT_1TO5 in {1..${FIRST}}
 do
     echo "Number: ${PRINT_1TO5}"
 done
 }
 
-FUNCTION 
 FUNCTION
