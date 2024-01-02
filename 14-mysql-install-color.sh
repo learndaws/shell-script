7cf06329f519d7f57f689e607a70576815ab5a0d
learndaws <learndaws@gmail.com>
2024-01-02 18:18:09 +0530

    exp push

---
+++ 14-mysql-install-color.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+B="\e[30m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+ROOTACCESSCHECK=$(id -u)
+
+
+DATE=$(date +%F)
+
+LOG="Jenkins_intall_${DATE}.log"  #even this also can be done LOG="/tmp/$0-${DATE}.log".. saves the file under tmp folder and $0 shows the name of the file
+
+
+EXIT_CHECK() 
+{
+    if [ $1 != 0 ];
+        then
+            echo "${R} $2 ${N}"
+            exit 1
+        else
+            echo "${G} $3 ${N}"
+fi
+}
+
+EXIT_CHECK "$ROOTACCESSCHECK" "FAILURE-1: you don't have root access" "SUCCESS-1: you have root access"
+
+
+yum install mysql -y  &>> ${LOG}
+
+EXIT_CHECK "$?" "FAILURE-2: Mysql installation is failed" "SUCCESS-2: Mysql installation is success"
+
+
+yum install git -y  &>> ${LOG}
+EXIT_CHECK "$?" "FAILURE-3: git installation is failed" "SUCCESS-3: git installation is success"
---
+++ 15-for-loop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for PRINT_1TO5 in {1..5}
+do
+    echo "Number: ${PRINT_1TO5}"
+done
