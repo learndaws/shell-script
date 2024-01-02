8daead4d9f54b79a27b0d7f10420727f9cbcf0f0
learndaws <learndaws@gmail.com>
2024-01-02 20:12:04 +0530

    exp push

--- 15-for-loop.sh
+++ 15-for-loop.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-for PRINT_1TO5 in {1..100}
+for PRINT_1TO5 in {1..5}
 do
     echo "Number: ${PRINT_1TO5}"
 done
 
-for PRINT_1TO5 in {1..100}
-do
-    echo "Number: ${PRINT_1TO5}"
-done
+#=============
+
+FUNCTION()
+{
+    for ((NUMBER=1; NUMBER<=$1; NUMBER++))
+    do
+        echo "Number: ${NUMBER}"
+    done
+}
+
+FUNCTION $1
+
+
+
