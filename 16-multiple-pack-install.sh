11bbcd2682a9c6c47160de4de1e03526bc47e4cb
learndaws <learndaws@gmail.com>
2024-01-03 17:21:39 +0530

    16

--- 16-multiple-pack-install.sh
+++ 16-multiple-pack-install.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-yum install $@ -y
+
+CH_SUDO_XS=$(id -u)
+
+
+VALIDATE()
+{
+   if [ $1 != 0 ]
+then
+    echo "$2"
+else
+    echo "$3"
+fi 
+}
+
+
+VALIDATE "${CH_SUDO_XS}" "You don't have sudo access" "You have sudo access"
+
+
+yum install git -y
+
+VALIDATE "$?" "git installation failed" "git installation successful"
+
