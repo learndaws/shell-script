2d708886646ec08a91646e5813d8a535007871ab
learndaws <learndaws@gmail.com>
2024-01-07 22:42:30 +0530

    renamed 17-delete-old-files.sh as 17-while-delete-old-files.sh and created 18

---
+++ 17-while-delete-old-files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FILE_PATH="/tmp/shell-scripts"
+
+if [ ! -d $FILE_PATH ]
+then
+    echo "file doesn't exist"
+fi
+
+FILE_TO_DELETE=$(find $FILE_PATH -type f -mtime +10 -name "*.js")
+
+while IFS= read -r line
+do 
+    echo "Deleting file: $line"
+    rm $line
+    echo "Deleted file: $line"
+done <<< $FILE_TO_DELETE
+
+
