da43e363bf5541ba8f51ec6c0d30f2044f9418e1
learndaws <learndaws@gmail.com>
2024-01-07 19:56:41 +0530

    17

--- 17-delete-old-files.sh
+++ 17-delete-old-files.sh
@@ -12,6 +12,8 @@ FILE_TO_DELETE=$(find $FILE_PATH -type f -mtime +10 -name "*.log")
 while IFS= read -r line
 do 
     echo "Deleting file: $line"
+    rm $line
+    echo "$line deleted"
 done <<< $FILE_TO_DELETE
 
 
