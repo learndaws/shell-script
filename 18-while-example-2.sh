4eeb12162062ee36e112b563fcf7d3e87cd47a30
learndaws <learndaws@gmail.com>
2024-01-07 23:23:31 +0530

    18

--- 18-while-example-2.sh
+++ 18-while-example-2.sh
@@ -4,11 +4,11 @@ INPUT_FILE=$(cat /etc/passwd)
 
 while IFS=':' read -r user_name password user_id group_id GECOS home_dir shell
 do 
-    echo "username: $user_name"
-    echo "password: $password" 
-    echo "user-id: $user_id"
-    echo "group-id: $group_id" 
-    echo "GECOS: $GECOS" 
-    echo "home-dir: $home_dir" 
-    echo "shell: $shell"
+    echo "username: $user_name"/
+    echo "password: $password"/
+    echo "user-id: $user_id"/
+    echo "group-id: $group_id"/ 
+    echo "GECOS: $GECOS"/ 
+    echo "home-dir: $home_dir"/ 
+    echo "shell: $shell"/
 done <<< $INPUT_FILE
