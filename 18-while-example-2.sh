9c0da97fab952eb50110f339faf964fd6b58b2df
learndaws <learndaws@gmail.com>
2024-01-07 22:52:59 +0530

    18

---
+++ 18-while-example-2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while IFR=':' read -r user_name password user_id group_id GECOS home_dir shell
+do 
+    echo "username: $user_name"
+    echo "password: $password" 
+    echo "user-id: $user_id"
+    echo "group-id: $group_id" 
+    echo "GECOS: $GECOS" 
+    echo "home-dir: $home_dir" 
+    echo "shell: $shell"
+done < /etc/passwd
