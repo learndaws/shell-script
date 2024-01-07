e2c33c054a2843e6f593df6ac0b7bd18d484d7c1
learndaws <learndaws@gmail.com>
2024-01-07 19:44:19 +0530

    17

--- 17-delete-old-files.sh
+++ 17-delete-old-files.sh
@@ -2,16 +2,16 @@
 
 FILE_PATH="/tmp/shell-scripts/"
 
-if [ ! -d ${FILE_PATH} ]
+if [ ! -d $FILE_PATH ]
 then
     echo "file doesn't exist"
 fi
 
-FILES_TO_DELETE="$(find ${FILE_PATH} -type f -mtime +10 -name "*.log")"
+FILES_TO_DELETE="$(find $FILE_PATH -type f -mtime +10 -name "*.log")"
 
 while IFS= read -r line
 do 
     echo "Deleting file: $line"
-done <<< ${FILE_TO_DELETE}
+done <<< $FILE_TO_DELETE
 
 
