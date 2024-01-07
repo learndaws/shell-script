98e2e9e9db5d006e7ffc24958c4c22e607764e28
learndaws <learndaws@gmail.com>
2024-01-07 19:50:46 +0530

    17

--- 17-delete-old-files.sh
+++ 17-delete-old-files.sh
@@ -7,7 +7,7 @@ then
     echo "file doesn't exist"
 fi
 
-FILES_TO_DELETE=$(find $FILE_PATH -type f -mtime +10 -name "*.log")
+FILE_TO_DELETE=$(find $FILE_PATH -type f -mtime +10 -name "*.log")
 
 while IFS= read -r line
 do 
