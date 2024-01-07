a000eca82d37c81645a0fa3bc12b6c821a72c804
learndaws <learndaws@gmail.com>
2024-01-07 23:16:54 +0530

    18

--- 18-while-example-2.sh
+++ 18-while-example-2.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-READ_FILE=$(cat 14-IP-Addresses)
+INPUT_FILE=$(cat /etc/passwd)
 
-while IFS= read -r line
+while IFS=':' read -r user_name password user_id group_id GECOS home_dir shell
 do 
-    nslookup $line | grep name &>> /tmp/new.log 
-
-done <<< $READ_FILE
+    echo "username: $user_name"
+    echo "password: $password" 
+    echo "user-id: $user_id"
+    echo "group-id: $group_id" 
+    echo "GECOS: $GECOS" 
+    echo "home-dir: $home_dir" 
+    echo "shell: $shell"
+done <<< $INPUT_FILE
