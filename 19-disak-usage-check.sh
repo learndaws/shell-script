6118e5dea42a76910c26459eec5b004198132f10
learndaws <learndaws@gmail.com>
2024-01-08 19:57:23 +0530

    19

--- 19-disak-usage-check.sh
+++ 19-disak-usage-check.sh
@@ -3,7 +3,7 @@
 DISK_USAGE_CHECK=$(df -hT | grep -vE 'mpfs|File' | awk '{print $6F}' | cut -d % -f1)
 echo "$DISK_USAGE_CHECK"
 
-if ($DISK_USAGE_CHECK -ge 1);
+if [ $DISK_USAGE_CHECK -ge 1 ];
 then
     echo "crossed threshold"
 else
