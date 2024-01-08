8c7c3b5fd771e550c459360127d2de6eb94ad30c
learndaws <learndaws@gmail.com>
2024-01-08 21:34:15 +0530

    19

--- 19-disak-usage-check.sh
+++ 19-disak-usage-check.sh
@@ -8,7 +8,7 @@ while IFS= read line
 do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
-        if [ $usage -ge $DISK_THRESHOLD ];
+        if [ $usage -ge 1 ];
         then        
             message+="Higher disk usage on $partition: $usage\n"
         fi 
