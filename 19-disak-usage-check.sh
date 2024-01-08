a31da9ad59da6797e4356e31327176471e3c7b7b
learndaws <learndaws@gmail.com>
2024-01-08 21:09:30 +0530

    19

--- 19-disak-usage-check.sh
+++ 19-disak-usage-check.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 INPUT=$(df -hT | grep -vE 'mpfs|File')
+DISK_THRESHOLD=1
 message=""
 
 while IFS= read line
 do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
-        if [ $usage -ge 1 ];
+        if [ $usage -ge $DISK_THRESHOLD ];
         then        
             message+="Higher disk usageon $partition: $usage\n"
         fi 
