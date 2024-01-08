6a4653f97c10986b3395075305d35259f3429c80
learndaws <learndaws@gmail.com>
2024-01-08 20:57:16 +0530

    19

--- 19-disak-usage-check.sh
+++ 19-disak-usage-check.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 INPUT=$(df -hT | grep -vE 'mpfs|File' | awk '{print $6F}' | cut -d % -f1)
-PARTITION_NAME=$(df -hT | grep -vE 'tmpfs|File' | awk '{print $1F}')
 
 while IFS= read -r line
 do
