46f99f13ab9de47ede467c0668102f1a2a957ae4
learndaws <learndaws@gmail.com>
2024-01-08 21:00:50 +0530

    19

--- 19-disak-usage-check.sh
+++ 19-disak-usage-check.sh
@@ -6,10 +6,9 @@ while IFS= read -r line
 do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
-        if [  $line -ge 1 ];
-        then
-        
-            echo "$line:crossed threshold"
+        if [  $usage -ge 1 ];
+        then        
+            message+="Higher disk usageon $partition: $usage\n"
         else
             echo "$line:under threshold"
     fi 
