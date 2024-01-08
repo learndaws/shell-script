04413218213023bdc1e1e371088c820e9422db63
learndaws <learndaws@gmail.com>
2024-01-08 20:56:38 +0530

    19

--- 19-disak-usage-check.sh
+++ 19-disak-usage-check.sh
@@ -7,8 +7,8 @@ while IFS= read -r line
 do
     if [  $line -ge 1 ];
     then
-        usage=$($line | awk '{print $6F}' | cut -d % -f1)
-        partition=$($line | awk '{print $1F}')
+        usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+        partition=$(echo $line | awk '{print $1F}')
         echo "$PARTITION_NAME:$line:crossed threshold"
     else
         echo "$PARTITION_NAME:$line:under threshold"
