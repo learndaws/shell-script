28771b2901d7dc6b5d65fa39d5373e26a7670b29
learndaws <learndaws@gmail.com>
2024-01-08 20:56:06 +0530

    19

--- 19-disak-usage-check.sh
+++ 19-disak-usage-check.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-DISK_USAGE_CHECK=$(df -hT | grep -vE 'mpfs|File' | awk '{print $6F}' | cut -d % -f1)
+INPUT=$(df -hT | grep -vE 'mpfs|File' | awk '{print $6F}' | cut -d % -f1)
 PARTITION_NAME=$(df -hT | grep -vE 'tmpfs|File' | awk '{print $1F}')
 
 while IFS= read -r line
 do
     if [  $line -ge 1 ];
     then
+        usage=$($line | awk '{print $6F}' | cut -d % -f1)
+        partition=$($line | awk '{print $1F}')
         echo "$PARTITION_NAME:$line:crossed threshold"
     else
         echo "$PARTITION_NAME:$line:under threshold"
 fi 
-done <<< $DISK_USAGE_CHECK 
+done <<< $INPUT 
